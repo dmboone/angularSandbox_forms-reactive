e4d100b84c4ebe64631f056de2ff16a6eed38fdb
Destiny Boone <destaj.db@gmail.com>
2022-11-08 12:07:59 -0500

    grouping controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,13 @@ export class AppComponent implements OnInit{
   ngOnInit(){
     this.signupForm = new FormGroup({ // creates a form
       // now adding form controls - key/value pairs
-      'username': new FormControl(null, Validators.required), // form control is done here rather than in the html file by using Validators
-      'email': new FormControl(null, [Validators.required, Validators.email]), // if multiple validators use array
       'gender': new FormControl('female'), // can set a default rather than choosing null
+
+      // creating a form group within the main form group
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required), // form control is done here rather than in the html file by using Validators
+        'email': new FormControl(null, [Validators.required, Validators.email]), // if multiple validators use an array
+      }),
     }); 
   }
 
