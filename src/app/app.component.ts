a3734998c1aa519082da18aa964803599cb6ff6e
Destiny Boone <destaj.db@gmail.com>
2022-11-08 11:17:13 -0500

    creating a form in code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  ngOnInit(){
+    this.signupForm = new FormGroup({ // creates a form
+      // now adding form controls - key/value pairs
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('female'), // can set a default rather than choosing null
+    }); 
+  }
 }
